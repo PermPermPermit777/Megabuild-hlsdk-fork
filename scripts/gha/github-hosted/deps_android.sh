6cc5b97ca198ac8e5dd39f7f1ea1a0b2c68284e0
PermPermPermit777 <bangconaaquenia0@gmail.com>
2026-09-19 10:23:00 +0800

    Update Ndk version

--- scripts/gha/github-hosted/deps_android.sh
+++ scripts/gha/github-hosted/deps_android.sh
@@ -3,10 +3,10 @@
 
 cd "$GITHUB_WORKSPACE" || exit 1
 
-wget https://dl.google.com/android/repository/android-ndk-r29-linux.zip
-unzip -x android-ndk-r29-linux.zip
-rm android-ndk-r29-linux.zip
-mv android-ndk-r29 ndk
+wget https://dl.google.com/android/repository/android-ndk-r30-linux.zip
+unzip -x android-ndk-r30-linux.zip
+rm android-ndk-r30-linux.zip
+mv android-ndk-r30 ndk
 
 git clone --recursive https://github.com/FWGS/hlsdk-portable
 
